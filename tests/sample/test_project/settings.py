ac706481a6b4c5e3542031973ba68d5bedbe3266
Iacopo Spalletti <i.spalletti@nephila.it>
2020-12-21 10:20:38 +0100

    Add support for codeql action (#16)

--- tests/sample/test_project/settings.py
+++ tests/sample/test_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "oelo@hsorsp1sxe^50%+h%p*)!uk4l!oc_urtlav9!&348tjhq"
+SECRET_KEY = "super-secret-string"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
